c3a230738bfa8282dcd94602167d9ffe3fcad4d6
ivan <ia.farafonov@yandex.ru>
2022-10-17 19:50:37 +0300

    Решена задача №25

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+/*Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"{a} в степение {b} равно {row(a,b)}");
+
+int row(int x, int y)
+{
+    int n = x;
+    for (int i = 1; i < y; i++)
+    {
+        n = n * x;
+    }
+    return n;
+}
+*/
